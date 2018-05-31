577dea81f16c6639c410e89b12bc7d489ff5bdcd
Ayllium <Ayllium1@gmail.com>
2018-05-31 17:25:26 -0400

    editted db login

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = { mongoURI: 'mongodb://<Ayllium>:<Coder28JsPy>@ds243728.mlab.com:43728/vidjot-prod' }
+  module.exports = { mongoURI: 'mongodb://Ayllium:Coder28JsPy@ds243728.mlab.com:43728/vidjot-prod' }
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
